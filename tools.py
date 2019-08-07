73f857973b0210243516a33de0e0b7f175cb1af5
toinsson <antoine.loriette@gmail.com>
2019-08-07 16:33:00 +0100

    update to notebooks

--- tools.py
+++ tools.py
@@ -38,3 +38,25 @@ def get_dataset(name):
         sys.exit(0)
 
     return (X_train, y_train, X_test, y_test)
+
+
+
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+def K_NN_classifier(data, labels, K=1):
+
+    split = sklearn.model_selection.ShuffleSplit(n_splits=1, random_state=0, test_size=0.25)
+    id_train, id_test = next(split.split(data))
+
+    nnc_train = data[id_train]
+    nnc_test  = data[id_test]
+
+    neigh = KNeighborsClassifier(n_neighbors=K)
+    neigh.fit(nnc_train, labels[id_train])
+
+    y_pred = neigh.predict(nnc_test)
+    y_true = labels[id_test]
+
+    return accuracy_score(y_true, y_pred)
